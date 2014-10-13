5b67c2b74ca08dc107356c70ec3474fc8f4e7f55
DannyOcean <dk.winni@gmail.com>
2014-10-13 19:27:46 +0300

    added tests for project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
+  subject(:project) { Project.new title: 'project' }
+
+  it "valid with title" do
+    expect(project).to be_valid
+  end
+
+  it "is valid with description length < 255 characters" do
+    project.description = "definitely higher" * 20
+    project2 = Project.new title: 'project2', description: '12345'
+    expect(project).to_not be_valid
+    expect(project2).to be_valid
+  end
 end
